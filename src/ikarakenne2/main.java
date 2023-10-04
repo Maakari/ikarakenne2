95f28a007dd3dcfb05938fe226a02c1860bf8291
Maakari <niklas.k.salmi@gmail.com>
2023-10-04 11:19:49 +0300

    Projekti luotu
    Luotu projekti sekä ensimmäinen versio

--- src/ikarakenne2/main.java
+++ src/ikarakenne2/main.java
@@ -4,7 +4,25 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int ika = 10;
 
+		// Tulostusehdot
+
+		if (ika >= 0 && ika < 18)
+
+		{
+
+		System.out.println("Olet alaikäinen");
+
+		}
+
+		else
+
+		{
+
+		System.out.println("Olet aikuinen");
+
+		}
 	}
 
 }
