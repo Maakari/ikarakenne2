1396ce47e5b2c8156ef690c83281e4495e4c0dc2
Maakari <niklas.k.salmi@gmail.com>
2023-10-04 11:27:26 +0300

    Tehty tehtävät 2-4
    Tehty tehtävänannon mukaan tehtävät 2-4.
    Tehtävä 2: Laajenna ohjelmaa niin, että se kertoo 18-vuotiaalle, että hän on juuri tullut täysi-ikäiseksi ja saa ajaa autoa. Tehtävä 3: Laajenna tehtävää niin, että aikuisille ilmoitetaan tasavuosikymmenistä onnentoivotus. Tehtävä 4: jos ikä on 100, tulosta 3-rivinen onnentoivotus

--- src/ikarakenne2/main.java
+++ src/ikarakenne2/main.java
@@ -4,28 +4,61 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int ika = 18;
+
+		int ika = 65;
 
 		// Tulostusehdot
 
-		if (ika >= 0 && ika < 18)
-
-		{
+		if (ika > 0 && ika < 18) {
 
 		System.out.println("Olet alaikäinen");
-		}
-		 if (ika >= 15) {
-
-			 System.out.println("Saat ajaa mopoa"); }
-		 if (ika >=16) {
-				System.out.println ("Saat ajella kevaria");
-
-		}
-
-		 else if (ika >= 65) { System.out.println("Olet eläkeläinen"); 
-		 } 
-		 
-		 else { System.out.println("Olet aikuinen"); }
+if (ika >= 15) {
+	System.out.println ("saat ajella mopoa");
+	if (ika >=16) {
+		System.out.println ("Saat ajella kevaria");
+		
 	}
 }
+		}
+		else if (ika == 18)
+		{
+		System.out.println ("Olet aikuinen ja saat ajaa autoa");
+	}
+		else if (ika >= 65)
+		{
+			System.out.println("Olet eläkeläinen");
+			if (ika == 65)
+			{
+				System.out.println ("Hyviä eläkepäiviä");
+			}
+		}
+			
+		else if (ika <= 100);
+			{
+				System.out.println ("onnea 100 vuodesta");
+				System.out.println ("onnea 100 vuodesta");
+				System.out.println ("onnea 100 vuodesta");
+			}
+	
+	
+			if (ika >58 && ika < 65) 
+		{
+			
+			System.out.println ("voit mennä varhaiseläkkeelle");
+		}
+			
+		else
+		{
+		System.out.println("Olet aikuinen");
+		
+
+		}
+	
+	
+	
+	}
+	
+	
+	}
+
 
